fc1050b770b15912241117cb04a957dd51a2b589
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-05-24 10:09:22 -0400

    Install dependencies and set up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Home from './pages/Home.js';
+import Portfolio from './pages/Portfolio.js';
+import News from './pages/News.js';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/news">Crypto News</Link>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/news' component={News} />
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
