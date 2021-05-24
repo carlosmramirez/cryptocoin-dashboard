0408431f004c97d187a163fa89833f4bf2242011
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-05-24 13:03:15 -0400

    Fetch data and populate CoinChart

--- src/components/CoinChart/CoinChart.js
+++ src/components/CoinChart/CoinChart.js
@@ -1,6 +1,6 @@
 import Chart from "react-google-charts";
 
-export default function CoinChart() {
+export default function CoinChart(props) {
   return (
     <div>
       <Chart
@@ -8,21 +8,7 @@ export default function CoinChart() {
         height={'400px'}
         chartType="LineChart"
         loader={<div>Loading Chart</div>}
-        data={[
-          ['x', 'bitcoin'],
-          [0, 0],
-          [1, 10],
-          [2, 23],
-          [3, 17],
-          [4, 18],
-          [5, 9],
-          [6, 11],
-          [7, 27],
-          [8, 33],
-          [9, 40],
-          [10, 32],
-          [11, 35],
-        ]}
+        data={props.data}
         options={{
           hAxis: {
             title: 'Date Range',
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import CoinChart from '../components/CoinChart/CoinChart.js';
 import WatchList from '../components/WatchList/WatchList.js';
 import CoinNews from '../components/CoinNews/CoinNews.js';
 
 export default function Home() {
+  const [chartData, setChartData] = useState('')
+
+  const currUnixTime = Math.floor(Date.now() / 1000); 
+  const pastUnixTime = Math.floor(Date.now() / 1000) - 604800; //7 days ago
+
+  useEffect(() => {
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${pastUnixTime}&to=${currUnixTime}`
+    )
+    .then(res => res.json())
+    .then(jsonRes => {
+      return (
+        jsonRes.prices.map(([time, price]) => {
+          return [timeConverter(time), price];
+        })
+      );
+    })
+    .then(formattedData => {
+      formattedData.unshift(['x', 'bitcoin']);
+      setChartData(formattedData);
+    })
+  },[])
+     
+  const timeConverter = (unixTime) => {
+    const date = new Date(unixTime).toLocaleDateString("en-US");
+    return date;
+  }
+  
   return (
     <div>
-      <CoinChart /> 
+      <CoinChart data={chartData}/> 
       <WatchList />
       <CoinNews />
     </div>
